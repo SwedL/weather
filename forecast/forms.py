a4179977cef04ff2adf050adab866ff21f287732
Alexey Osminin <supersega@mail.ru>
2024-07-18 11:58:24 +0500

    create form and front

---
+++ forecast/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class CityNameForm(forms.Form):
+    """ Форма ввода названия города по которому необходимо получить прогноз погоды """
+
+    city = forms.CharField(max_length=150, required=True,
+                           widget=forms.TextInput(attrs={'placeholder': 'введите название города'}))
--- forecast/views.py
+++ forecast/views.py
@@ -1,43 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views import View
+from .forms import CityNameForm
 
 
 class WeatherForecastView(View):
-    title = 'Прогноз погоды по выбранному городу'
+    title = 'Прогноз погоды'
+    form_class = CityNameForm
 
     def get(self, request):
 
         context = {
-            'employees': page_obj,
-            'form': form,
-            'paginator_range': page_obj.paginator.get_elided_page_range(page_obj.number),
-            'common_form_data': common_form_data,
+            'form': self.form_class,
             'title': self.title,
-            'staff': request.user.has_perm('forecast.change_employee'),
         }
 
-        return render(request, 'forecast/department.html', context=context)
+        return render(request, 'forecast/main.html', context=context)
 
     def post(self, request):
 
-        form = SearchEmployeeForm(request.POST)
-
-        # обновление словаря common_form_data
-        if form.is_valid():
-            common_form_data.update(form.cleaned_data)
-
-
         context = {
-            'employees': page_obj,
-            'form': form,
-            'paginator_range': page_obj.paginator.get_elided_page_range(page_obj.number),
-            'common_form_data': common_form_data,
-            'title': self.title,
-            'staff': request.user.has_perm('forecast.change_employee'),
+            'form': self.form_class,
+            # 'common_form_data': common_form_data,
+            'title': 'Прогноз ГОТОВ',
         }
 
-        return render(request, 'forecast/department.html', context=context)
+        return render(request, 'forecast/main.html', context=context)
 
 
 def pageNotFound(request, exception):
