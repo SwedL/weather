924c0f27c4809c795b2281dedbd06c052b2b5aad
Alexey Osminin <supersega@mail.ru>
2024-07-19 00:30:48 +0500

    add alert message "error get data"

--- forecast/views.py
+++ forecast/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseNotFound
 from django.views import View
 from .forms import CityNameForm
 from datetime import date, timedelta
